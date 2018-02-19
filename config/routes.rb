b0e3bb08ed10de30d930a1b81269efbada8675ca
Claudine Pradal <claudinecartailler@gmail.com>
2018-02-19 15:59:55 +0100

    Add all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'deliveries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :deliveries, only: [:show, :new, :create] do
+    resources :bookings, only: [:create]
+  end
+
+  namespace :my do
+    resources :deliveries, only: [:index] do
+      member do
+        post 'done'
+      end
+    end
+  end
+
+  resources :bookings, only: [:index, :destroy]
 end
