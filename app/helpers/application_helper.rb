f68e1609693c109995a3939af86335147ca78589
Claudine Pradal <claudinecartailler@gmail.com>
2018-02-20 15:58:01 +0100

    Add of my Bookings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def delivery_color_status(status)
+    if status == "done"
+      'c-green'
+    elsif status == 'in progress'
+      'c-blue'
+    else
+      'c-orange'
+    end
+  end
 end
