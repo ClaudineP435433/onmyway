0e8d895b1a193e2f37dfeeab4f343cdf2ba757ac
Victor Sassonia <victor.sassonia@gmail.com>
2018-02-20 18:53:34 +0100

    map on deliveries/show

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -16,6 +16,14 @@ class DeliveriesController < ApplicationController
 
   def show
     @booking = Booking.new
+    @restaurant = @delivery.restaurant
+
+    @markers = [
+      {
+        lat: @restaurant.latitude,
+        lng: @restaurant.longitude
+      }
+    ]
   end
 
   def new
