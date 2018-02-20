3e19405a4b6294e3bdb7c068f2397e2a254c1d49
Alexandre Gros <xelaflash@gmail.com>
2018-02-20 10:53:37 +0100

    seed ok

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,8 @@
 class Category < ApplicationRecord
   has_many :restaurants
-  CATEGORIES = ["Kebab", "Chinese", "Italian", "French", "Burger", "Japanese", "Snack Bar", "Indian"]
-  validates :name, inclusion: {
-    in: CATEGORIES,
-    message: "%{value} is not a valid category"
-  }
+  # CATEGORIES = ["Kebab", "Chinese", "Italian", "French", "Burger", "Japanese", "Snack Bar", "Indian"]
+  # validates :name, inclusion: {
+  #   in: CATEGORIES,
+  #   message: "%{value} is not a valid category"
+  # }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,68 @@
 puts "start seed"
 
-Restaurant.destroy_all
+Booking.destroy_all
+Delivery.destroy_all
 User.destroy_all
+Restaurant.destroy_all
+Category.destroy_all
+
+
+category = ["Kebab", "Chinese", "Italian", "French", "Burger", "Japanese", "Snack Bar", "Indian"]
+
+category.each { |cat| Category.create(name: cat) }
+
+puts "cat ok"
+
+restaurants_data = [
+  {
+    name: "Mc donalds",
+    category_id: Category.first.id,
+    address: "26 rue de mes roustons  33000 bordeaux",
+    picture: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2016/02/26/103423796-McPick2-FOF-Header.1910x1000.jpg"
+    },
+
+  {
+    name: "Pizza nico",
+    category_id: Category.second.id,
+    address: "26 avenue des champs  33000 bordeaux",
+    picture: "https://image.afcdn.com/recipe/20160519/15342_w600.jpg"
+    },
+
+  {
+    name: "Chez maitre Tang",
+    category_id: Category.third.id,
+    address: "5 rue monge  75000 paris",
+    picture: "https://hanoitourist.info/wp-content/uploads/2017/01/tet-4.jpg"
+    },
+
+  {
+    name: "Curry folies",
+    category_id: Category.last.id,
+    address: "33 avenue des champs  33000 bordeaux",
+    picture: "https://fthmb.tqn.com/sMS4T7yea_uM00Gg2RAA5ORfiSg=/2121x1414/filters:fill(auto,1)/GettyImages-169944128-587af9fe3df78c17b636d4f2.jpg"
+    },
+
+  {
+    name: "Le bouchon",
+    category_id: Category.second.id,
+    address: "26 rue colonel reyel 33000 bordeaux",
+    picture: "http://www.unlyondanslacuisine.fr/Wordpress/wp-content/uploads/2014/11/Bouchon-lyonnais.jpg"
+    },
+
+  {
+    name: "Keboul Wesh",
+    category_id: Category.third.id,
+    address: "3 avenue mes boules, 33000 bordeaux",
+    picture: "https://static2.uk.businessinsider.com/image/5a2275e1c1bde0e0548b5701-2000/shutterstock202575529.jpg"
+    },
+]
+
+  restaurants_data.each do |resto|
+    Restaurant.create!(resto)
+  end
+
+
+puts "puts user"
 
 10.times do
   user = User.new(
@@ -14,45 +75,28 @@ User.destroy_all
   user.save!
 end
 
-restaurants = [
-  {
-    name: "Mc donalds",
-    address: "26 rue de mes roustons  33000 bordeaux",
-    picture: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2016/02/26/103423796-McPick2-FOF-Header.1910x1000.jpg?v=1456506290"
-    },
+puts "deliv"
 
-  {
-    name: "Pizza nico",
-    address: "26 avenue des champs  33000 bordeaux",
-    picture: "https://image.afcdn.com/recipe/20160519/15342_w600.jpg"
-    },
+6.times do
+  deliv = Delivery.new(
+  restaurant_id: (Restaurant.first.id..Restaurant.last.id).to_a.sample,
+  user_id: (User.first.id..User.last.id).to_a.sample,
+  closing_at: DateTime.now,
+  )
+  deliv.save!
+end
 
-  {
-    name: "Chez maitre Tang",
-    address: "5 rue monge  75000 paris",
-    picture: "https://hanoitourist.info/wp-content/uploads/2017/01/tet-4.jpg"
-    },
+puts "bookings"
 
-  {
-    name: "Curry folies",
-    address: "33 avenue des champs  33000 bordeaux",
-    picture: "https://fthmb.tqn.com/sMS4T7yea_uM00Gg2RAA5ORfiSg=/2121x1414/filters:fill(auto,1)/GettyImages-169944128-587af9fe3df78c17b636d4f2.jpg"
-    },
+3.times do
+  book = Booking.new(
+    user_id: (User.first.id..User.last.id).to_a.sample,
+    delivery_id: (Delivery.first.id..Delivery.last.id).to_a.sample,
+    content: "Maxi menu best-of BigMac pour 4 EUR",
+    )
+  book.save!
+end
 
-  {
-    name: "Le bouchon",
-    address: "26 rue colonel reyel 33000 bordeaux",
-    picture: "http://www.unlyondanslacuisine.fr/Wordpress/wp-content/uploads/2014/11/Bouchon-lyonnais.jpg"
-    },
-
-  {
-    name: "Keboul Wesh",
-    address: "3 avenue mes boules, 33000 bordeaux",
-    picture: "https://static2.uk.businessinsider.com/image/5a2275e1c1bde0e0548b5701-2000/shutterstock202575529.jpg"
-    },
-]
-
-restaurants.each { |restaurant| Restaurant.create(restaurant) }
 
 
 
