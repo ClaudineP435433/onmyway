260950f2c076314fc9268fe9bc2fb842a935fb48
Claudine Pradal <claudinecartailler@gmail.com>
2018-02-21 11:10:19 +0100

    Add of User Lat & Lng

--- app/controllers/my/deliveries_controller.rb
+++ app/controllers/my/deliveries_controller.rb
@@ -1,6 +1,18 @@
 class My::DeliveriesController < ApplicationController
   def index
     @deliveries = Delivery.where(user_id: current_user)
+
+    @user_delivery_guy = current_user
+    @user_delivery_guy_marker = [{ lat: @user_delivery_guy.latitude, lng: @user_delivery_guy.longitude }]
+
+    @usermarkers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+    end
+
   end
 
   def done
