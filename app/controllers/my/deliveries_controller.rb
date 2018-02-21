0a9bff5eea7da37f4917bd052173152ff80e8e37
Claudine Pradal <claudinecartailler@gmail.com>
2018-02-21 15:59:37 +0100

    Add of User Controller & co

--- app/controllers/my/deliveries_controller.rb
+++ app/controllers/my/deliveries_controller.rb
@@ -1,18 +1,6 @@
 class My::DeliveriesController < ApplicationController
   def index
     @deliveries = Delivery.where(user_id: current_user)
-
-    @user_delivery_guy = current_user
-    @user_delivery_guy_marker = [{ lat: @user_delivery_guy.latitude, lng: @user_delivery_guy.longitude }]
-
-    @usermarkers = @flats.map do |flat|
-      {
-        lat: flat.latitude,
-        lng: flat.longitude#,
-        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
-      }
-    end
-
   end
 
   def done
@@ -21,5 +9,31 @@ class My::DeliveriesController < ApplicationController
     @delivery.save
     redirect_to my_deliveries_path
   end
+
+  def show
+    # pour l'adresse du livreur
+    @user_delivery_guy = current_user
+    @user_delivery_guy_marker = [{ lat: @user_delivery_guy.latitude, lng: @user_delivery_guy.longitude }]
+
+
+    # pour l'adresse des clients
+    @delivery = Delivery.find(params[:id])
+    @bookings = Booking.where(delivery_id: @delivery)
+    @users_customers = @bookings.map do |booking|
+      User.find(booking.user_id)
+    end
+    @users_customers_markers = @users_customers.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+    end
+
+    # pour l'adresse du resto
+    @restaurant = @delivery.restaurant
+    @restaurant_marker = [{ lat: @restaurant.latitude, lng: @restaurant.longitude }]
+  end
+
 
 end
