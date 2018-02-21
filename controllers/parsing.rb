1c13432e64374537acc4b87fdda45c27c9da96a3
Alexandre Gros <30756484+Xelaflash@users.noreply.github.com>
2018-02-21 18:41:42 +0100

    Update parsing.rb

--- controllers/parsing.rb
+++ controllers/parsing.rb
@@ -1,13 +1,1 @@
-require 'open-uri'
-require 'nokogiri'
 
-ville = 'bordeaux'
-url = "https://www.yelp.fr/search?find_desc=Restaurants&find_loc=Bordeaux&start=60"
-
-html_file = open(url).read
-html_doc = Nokogiri::HTML(html_file)
-
-html_doc.search('.m_titre_resultat a').each do |element|
-  puts element.text.strip
-  puts element.attribute('href').value
-end
