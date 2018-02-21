b8e2490501bf703fc0111382f78d63b15e6fd2ac
Victor Sassonia <victor.sassonia@gmail.com>
2018-02-21 14:34:55 +0100

    adding validations on date bookings

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -3,6 +3,7 @@ class Delivery < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :closing_at, presence: true
+  validate :future_date
 
   def status
     if delivered_at.present?
@@ -14,4 +15,7 @@ class Delivery < ApplicationRecord
     end
   end
 
+  def future_date
+     errors.add(:closing_at, "can't be in the past") if closing_at.past?
+  end
 end
