a4b5f05b02462ae904d2537eaa2eae510838ae71
Claudine Pradal <claudinecartailler@gmail.com>
2018-02-23 14:11:33 +0100

    Add Search only on index

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Onmyway
       generate.assets false
       generate.helper false
     end
-
+    config.action_view.embed_authenticity_token_in_remote_forms = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
